b2e60d58c623192ffb800a21878572d56fa84220
IsraelBosun <israelbosun1@gmail.com>
2022-12-02 21:53:24 +0100

    Did some fascinating things with the cards using JS while making cart

--- main.js
+++ main.js
@@ -18,3 +18,60 @@ hamburger.addEventListener("click", hamNav)
 console.log(navMenu)
 console.log(hamburger)
 console.log()
+
+
+let shop = document.getElementById("shop")
+let shopItemsData = [{
+    id: "first",
+    name: "ZHIYUN SMOOTH-X",
+    price: 45000,
+    desc: "Experience the extra life",
+    img: "index-images/smx-banner-detail-img1en.png"
+},
+{
+    id: "second",
+    name: "DJI OSMO",
+    price: 50000,
+    desc: "Experience the extra life",
+    img: "index-images/o2.png"
+},
+{
+    id: "third",
+    name: "ZHIYUN WEEBILL-S",
+    price: 60000,
+    desc: "Experience the extra life",
+    img: "index-images/03.png"
+},
+{
+    id: "forth",
+    name: "DJI RONIN-SC",
+    price: 70000,
+    desc: "Explore Your Xtra Life",
+    img: "index-images/04.png"
+}]
+let generateShop = ()=>{
+    return (shop.innerHTML = shopItemsData.map((x)=>{
+        let {id, name, price, desc, img } = x
+        return `
+        <div id= product-id-${id} class="item col-lg-3 col-xl-2 col-md-4 col-sm-5 col-xs-1">
+            <div class="mt-3 details">
+              <p class="zhi">${name}</p>
+              <p class="explore mt-0">${desc}</p>
+            </div>
+            <div class="image-div">
+            <img class="imagery" width="150" src="${img}" alt="">
+            </div>
+            <div class="mt-2 price-quantity">
+              <h4># ${price}</h4>
+              <div class="buttons">
+                <i id="decrease" class="signature bi bi-dash-lg"></i>
+                <div class="quantity">0</div>
+                <i id="increase" class="signature bi bi-plus-lg"></i>
+              </div>
+            </div>
+          </div>
+        `
+    }).join(""));
+};
+
+generateShop()
